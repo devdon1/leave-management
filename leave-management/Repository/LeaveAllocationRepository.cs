03322b38e4323eb6f2b3b8dfa67707238072ad3c
Dev Don <devdon1@outlook.com>
2020-01-22 21:56:12 +0100

    Added Contracts (Interfaces) and Repositories

---
+++ leave-management/Repository/LeaveAllocationRepository.cs
@@ -0,0 +1,49 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveAllocationRepository : ILeaveAllocationRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public LeaveAllocationRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool Create(LeaveAllocation entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(LeaveAllocation entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveAllocation> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeaveAllocation FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(LeaveAllocation entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leave-management/Repository/LeaveHistoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveHistoryRepository : ILeaveHistoryRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public LeaveHistoryRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool Create(LeaveHistory entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(LeaveHistory entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveHistory> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeaveHistory FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(LeaveHistory entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
